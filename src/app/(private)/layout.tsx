0f0070bdd27aacef238b7d7a068cf50d1d6f10f5
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2024-10-23 08:36:10 -0400

    layout

---
+++ src/app/(private)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+export default function PrivateLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <header></header>
+
+      <main className="container my-6 mx-auto">{children}</main>
+    </>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+	container: {
+		center: true,
+		padding: "2rem",
+		screens: {
+			"2xl": "1400px",
+		},
+	},
   	extend: {
 		fontFamily:{
 			sans:["var(--font-sans)", ...fontFamily.sans]
@@ -60,7 +67,7 @@ const config: Config = {
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
   		}
-  	}
+  	},
   },
   plugins: [require("tailwindcss-animate")],
 };
